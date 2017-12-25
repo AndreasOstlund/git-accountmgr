8be5a99b04586d52caa242afe998005fe62d46f4
AndreasOstlund <AndreasOstlund@users.noreply.github.com>
2017-12-25 21:49:52 +0100

    Roughly working

--- git-accountmgr.py
+++ git-accountmgr.py
@@ -3,6 +3,8 @@
 
 import ConfigParser
 import subprocess
+import sys
+import os
 
 
 
@@ -13,10 +15,13 @@ class GitConfig(object):
         
     def call_git(self, params):
         
+        if type(params) is str:
+            params = tuple(params.split(' '))
+
         # https://stackoverflow.com/questions/2473655/how-to-make-a-call-to-an-executable-from-python-script
-        popen = subprocess.Popen(params, stdout=subprocess.PIPE)
+        popen = subprocess.Popen( ('/usr/bin/git',)+params, stdout=subprocess.PIPE)
         popen.wait()
-        output = popen.stdout.read()
+        output = popen.stdout.read().strip()
         
         return {
             "returncode": popen.returncode
@@ -26,14 +31,58 @@ class GitConfig(object):
 
     def get_local_user_config(self):
         
-        self.call_git('config --local user.name')
+        output = self.call_git( 'config --local user.name' )
+        if output['returncode'] == 0:
+            self.username = output['stdout']
+        else:
+            self.username = ''
+
+        output = self.call_git( 'config --local user.email' )
+        if output['returncode'] == 0:
+            self.useremail = output['stdout']
+        else:
+            self.useremail = ''
 
 
 
-def main():
-    pass
+
+'''
+cmdparser = argparse.ArgumentParser(description='...')
+    cmdparser.add_argument('add', nargs="?", help='Adds an account')
+    cmdparser.add_argument('change', nargs="?", help='Chages account setting')
+    cmdparser.add_argument('set', nargs="?", help='Sets the account to use')
+
+    cmdargs = cmdparser.parse_args()
+
+'''
+
+
+def main(argv):
+    print len(argv)
+    print argv
+    print os.path.basename(argv[0])
+
+    called_as = os.path.basename(argv[0]).lower()
+
+    if called_as == 'git-accountmgr':
+        pass
+    elif called_as == 'pre-commit':
+        pass
+
+        gitconf = GitConfig()
+        gitconf.get_local_user_config()
+
+        print gitconf.username
+        print gitconf.useremail
+
+        if gitconf.username and gitconf.useremail:
+            sys.exit(os.EX_OK)
+        else:
+            print "No account specified "
+            sys.exit(os.EX_NOPERM)
+
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
 
